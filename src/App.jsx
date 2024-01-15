a21220ba93016fb33f6b9401b81d1f4fd54cd76c
Pia Wolffram <pia.wolffram@posteo.de>
2024-01-16 00:17:57 +0100

    new svgs

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,6 @@ function App() {
         }
         const data = await response.json();
         setPlaces(data.places);
-        console.log('data fetched!')
       } catch (error) {
         console.error('Error fetching data:', error);
       }
